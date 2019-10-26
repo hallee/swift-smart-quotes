ee37642dddca031237b216dc42cc37748e47ce2e
Hal Lee <hal@lee.me>
2019-10-26 16:17:07 -0400

    Implement localized replacements; Finnish, Swedish

--- Sources/SmartQuotes.swift
+++ Sources/SmartQuotes.swift
@@ -7,33 +7,41 @@ public extension String {
     }
 
     func withSmartQuotes(_ locale: Locale?) -> String {
+        let openApostrophe = Mark.openApostrophe.localized(locale)
+        let closeApostrophe = Mark.closeApostrophe.localized(locale)
+        let openQuote = Mark.openQuotation.localized(locale)
+        let closeQuote = Mark.closeQuotation.localized(locale)
+        let prime = Mark.prime.localized(locale)
+        let doublePrime = Mark.doublePrime.localized(locale)
+        let triplePrime = Mark.triplePrime.localized(locale)
+
         var string = self
         /// 'n'
-        string = string.replacingOccurrences(of: #"'n'"#, with: "’n’", options: .regularExpression)
+        string = string.replace(#"'n'"#, with: "\(closeApostrophe)n\(closeApostrophe)")
         /// triple prime
-        string = string.replace("'''", with: "‴")
+        string = string.replace("'''", with: "\(triplePrime)")
         /// beginning "
-        string = string.replace(#"(\W|^)"(\S)"#, with: "$1“$2")
+        string = string.replace(#"(\W|^)"(\S)"#, with: "$1\(openQuote)$2")
         /// ending "
-        string = string.replace(#"(“[^"]*)"([^"]*$|[^“"]*“)"#, with: "$1”$2")
+        string = string.replace(#"(“[^"]*)"([^"]*$|[^“"]*“)"#, with: "$1\(closeQuote)$2")
         /// remaining " at end of word
-        string = string.replace(#"([^\d])""#, with: "$1”")
+        string = string.replace(#"([^\d])""#, with: "$1\(closeQuote)")
         /// double prime
-        string = string.replace("''", with: "″")
+        string = string.replace("''", with: "\(doublePrime)")
         /// year abbreviations
-        string = string.replace(#"'(\d\ds?)"#, with: #"’$1"#)
+        string = string.replace(#"'(\d\ds?)"#, with: "\(closeApostrophe)$1")
         /// beginning '
-        string = string.replace(#"(\W|^)'(\S)"#, with: "$1‘$2")
+        string = string.replace(#"(\W|^)'(\S)"#, with: "$1\(openApostrophe)$2")
         /// possession
-        string = string.replace(#"([a-z])'([a-z])"#, with: "$1’$2")
+        string = string.replace(#"([a-z])'([a-z])"#, with: "$1\(closeApostrophe)$2")
         /// ending '
-        string = string.replace(#"((‘[^']*)|[a-z])'([^\d]|$)"#, with: "$1’$3")
+        string = string.replace(#"((‘[^']*)|[a-z])'([^\d]|$)"#, with: "$1\(closeApostrophe)$3")
         /// backwards apostrophe
-        string = string.replace(#"(\B|^)‘(?=([^‘’]*’\b)*([^‘’]*\B\W[‘’]\b|[^‘’]*$))"#, with: "$1’")
+        string = string.replace(#"(\B|^)‘(?=([^‘’]*’\b)*([^‘’]*\B\W[‘’]\b|[^‘’]*$))"#, with: "$1\(closeApostrophe)")
         /// double prime
-        string = string.replace("\"", with: "″")
+        string = string.replace("\"", with: "\(doublePrime)")
         /// prime
-        string = string.replace("'", with: "′")
+        string = string.replace("'", with: "\(prime)")
         return string
     }
 
@@ -48,7 +56,7 @@ public extension String {
         )
     }
 
-    private enum Mark: Equatable {
+    private enum Mark {
         case openApostrophe
         case closeApostrophe
         case openQuotation
@@ -61,6 +69,7 @@ public extension String {
             switch self {
             case .openApostrophe:
                 switch locale?.languageCode {
+                case "fi", "sv": return "’"
                 default: return "‘"
                 }
             case .closeApostrophe:
@@ -69,6 +78,7 @@ public extension String {
                 }
             case .openQuotation:
                 switch locale?.languageCode {
+                case "fi", "sv": return "”"
                 default: return "“"
                 }
             case .closeQuotation:
